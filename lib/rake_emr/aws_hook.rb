0c4e9c48f9b182cbbbdddea7c481f62454a8bffa
chenkovsky <chenkov@yeah.net>
2016-04-05 15:16:35 +0800

    update

--- lib/rake_emr/aws_hook.rb
+++ lib/rake_emr/aws_hook.rb
@@ -18,11 +18,17 @@ module RakeEmr
             RakeEmr.set_cluster cluster_id, master_dns
         end
         if RakeEmr.on_aws?
+            dir_occupied = %w{bin conf etc hive libexec pig Cascading-2.5-SDK contrib hadoop-examples.jar lib mahout sbin share}
             RakeEmr.script_dirs.each do |d|
+                raise "Dir name #{d} is occupied by aws." if dir_occupied.include? d.gsub(/\/+$/, "").split()[-1]
                 cmd = "scp -i #{RakeEmr.ssl_ca_file} -o StrictHostKeyChecking=no -r #{d} hadoop@#{RakeEmr.master_name}:~/"
                 STDERR.puts "executing:#{cmd}"
                 `#{cmd}`
             end
+            # STDERR.puts "#### opening proxy port for webhdfs ####"
+            # cmd = "ssh -i #{RakeEmr.ssl_ca_file} -o StrictHostKeyChecking=no hadoop@#{RakeEmr.master_name} -D #{RakeEmr.local_proxy_socket_port}"
+            # STDERR.puts "executing:#{cmd}"
+            # Process.fork { system cmd } # run ssh proxy in background
         end
         if RakeEmr.init_task
             RakeEmr.init_task.call
